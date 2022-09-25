66f36f0ca020772a5367a9611cbef59ffa3cc80b
David-DTA <tuananh28121999@gmail.com>
2022-09-25 17:36:57 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -5,26 +5,31 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')));
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
 //Http logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname,'resources','views'))
-
-//router
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}`)
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 );
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+//router init
+route(app);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
